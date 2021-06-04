c65fde540618fbece18a1436c8b089574d07dcbc
Isa <74831265+IsaAgverdiev@users.noreply.github.com>
2021-06-05 02:17:30 +0300

    add modalWindow

--- app/js/allProjects.js
+++ app/js/allProjects.js
@@ -1,13 +1,25 @@
 const girdProjects = document.querySelector(".grid-container__projects");
 
-girdProjects.addEventListener('click', openModal)
+if (girdProjects) {
+  girdProjects.addEventListener("click", openModal);
+}
 
 function openModal(e) {
-  let target = e.target.closest('.project') 
-  console.log(target);
+  let target = e.target.closest(".project");
 
-  // if(target.classList.contains())
-  
-  
+  if (target.classList.contains("project")) {
+    createModal();
+  }
+}
 
+function createModal() {
+  new WinBox("Modal Window", {
+    // modal: true,
+    background: "#1d1d1d",
+    html: "",
+    x: "center",
+    y: "center",
+    width: "50%",
+    height: "80%",
+  });
 }
