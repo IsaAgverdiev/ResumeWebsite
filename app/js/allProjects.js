e5f311586f13ff65e6c2d2abf59aa47f3e8f68ce
Isa <isamo.agverdiev@gmail.com>
2021-07-10 20:44:55 +0300

    refactoring mainPage

--- app/js/allProjects.js
+++ app/js/allProjects.js
@@ -1,38 +1,35 @@
-const girdProjects = document.querySelector(".grid-container__projects");
-const projectModal = document.querySelector(".project-modal");
-const projectModalInner = document.querySelector(".project-modal__inner");
-
-if (girdProjects) {
-  girdProjects.addEventListener("click", getModal);
-}
+const girdProjects = document.querySelector('.grid-container__projects');
+const projectModal = document.querySelector('.project-modal');
+const projectModalInner = document.querySelector('.project-modal__inner');
+const mainProjects = document.querySelectorAll('.project');
 
 function getModal(e) {
-  let target = e.target.closest(".project");
-  let targetName = target.getAttribute("data-name");
+  let target = e.target.closest('.project');
+  let targetName = target.getAttribute('data-name');
 
   switch (targetName) {
-    case "PureBank":
-      createModal("PureBank");
+    case 'PureBank':
+      createModal('PureBank');
       openModal();
       break;
-    case "Shirogo":
-      createModal("Shirogo");
+    case 'Shirogo':
+      createModal('Shirogo');
       openModal();
       break;
-    case "MyOffice":
-      createModal("MyOffice");
+    case 'MyOffice':
+      createModal('MyOffice');
       openModal();
       break;
-    case "ResumeWebsite":
-      createModal("ResumeWebsite");
+    case 'ResumeWebsite':
+      createModal('ResumeWebsite');
       openModal();
       break;
-    case "Desire":
-      createModal("Desire");
+    case 'Desire':
+      createModal('Desire');
       openModal();
       break;
-    case "KoreaCrem":
-      createModal("KoreaCrem");
+    case 'KoreaCrem':
+      createModal('KoreaCrem');
       openModal();
       break;
   }
@@ -47,7 +44,9 @@ function createModal(WebsiteName) {
       <span class="project-modal__status">статус проекта: ${projects[WebsiteName].status}</span>
       <div class="project-modal__links">
         <a class="project-modal__link project-modal__website" href=${projects[WebsiteName].URL} target="_blank">Сайт</a>
-        <a class="project-modal__link project-modal__code" href=${projects[WebsiteName].repositories} target="_blank">Код</a>
+        <a class="project-modal__link project-modal__code" href=${
+          projects[WebsiteName].repositories
+        } target="_blank">Код</a>
       </div>
       <p class="project-modal__discr">${projects[WebsiteName].discr}</p>
       <div class="project-modal__task">
@@ -56,13 +55,27 @@ function createModal(WebsiteName) {
       </div>
       <div class="project-modal__work">
         <h2 class="project-modal__work-title">Что было сделано:</h2>
-        ${`<ul class="project-modal__work-list">${workList.map((i) => `<li class="project-modal__work-item">${i}</li>`).join("")}</ul>`}
+        ${`<ul class="project-modal__work-list">${workList
+          .map((i) => `<li class="project-modal__work-item">${i}</li>`)
+          .join('')}</ul>`}
       </div>
       <div class="project-modal__tools">
         <h2 class="project-modal__tools-title">Инструменты и стек технологий</h2>
-        ${`<ul class="project-modal__tools-list">${toolsList.map((i) => `<li class="project-modal__tools-item">${i}</li>`).join("")}</ul>`}
+        ${`<ul class="project-modal__tools-list">${toolsList
+          .map((i) => `<li class="project-modal__tools-item">${i}</li>`)
+          .join('')}</ul>`}
       </div>
       `;
 
   projectModalInner.innerHTML = templates;
 }
+
+if (girdProjects) {
+  girdProjects.addEventListener('click', getModal);
+}
+
+mainProjects.forEach((i) => {
+  i.addEventListener('click', (e) => {
+    getModal(e);
+  });
+});
--- app/js/projectModal.js
+++ app/js/projectModal.js
@@ -1,13 +1,23 @@
-const projectModalBtnClose = document.querySelector(".project-modal__btn");
-
-if (projectModalBtnClose) {
-  projectModalBtnClose.addEventListener("click", closeModal);
-}
-
+const projectModalBtnClose = document.querySelector('.project-modal__btn');
+const projectBody = document.querySelector('.project-modal__body');
 
 function openModal() {
-  projectModal.classList.remove("hide");
+  projectModal.style.display = 'flex';
+  disableScroll();
 }
 function closeModal() {
-  projectModal.classList.add("hide");
+  projectModal.style.display = 'none';
+  enableScroll();
 }
+
+if (projectModalBtnClose) {
+  projectModalBtnClose.addEventListener('click', closeModal);
+}
+
+projectModal.addEventListener('click', (e) => {
+  let target = e.target;
+  if (target.matches('.project-modal')) {
+    closeModal();
+    enableScroll();
+  }
+});
