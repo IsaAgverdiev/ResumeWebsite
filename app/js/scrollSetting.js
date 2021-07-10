acdaba9a1b6d6aac0c96c03ccd2ba7ba30af8288
Isa <isamo.agverdiev@gmail.com>
2021-07-10 20:33:07 +0300

    refact mainProj, skillsBlock

---
+++ app/js/scrollSetting.js
@@ -0,0 +1,19 @@
+function disableScroll() {
+  const widthScroll = window.innerWidth - document.body.offsetWidth;
+
+  document.body.dbScrollY = window.scrollY;
+
+  document.body.style.cssText = `
+    position: fixed;
+    top: ${-window.scrollY}px;
+    left: 0;
+    width: 100%;
+    height: 100vh;
+    overflow: hidden;
+    padding-right: ${widthScroll}px
+  `;
+}
+function enableScroll() {
+  document.body.style.cssText = ``;
+  window.scroll({ top: document.body.dbScrollY });
+}
